c9f3731e2ef3d18e81e8a7a2b854810d802297ea
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2025-06-10 21:34:52 +1000

    feat: completed input section

--- CafeteriaMenuAnalysis.java
+++ CafeteriaMenuAnalysis.java
@@ -8,6 +8,7 @@ public class CafeteriaMenuAnalysis {
         // 1️⃣ INPUT SECTION
         // Input the number of items
         int numMenuItems = 0;
+        // Do...while loop for validation
         do {
             System.out.print("Enter the number of menu items: ");
             numMenuItems = scanner.nextInt();
@@ -15,6 +16,31 @@ public class CafeteriaMenuAnalysis {
                 System.out.println("Error: Number of menu items must be greater than zero.");
             }
         } while (numMenuItems <= 0);
+
+        int[] purchases = new int [numMenuItems];
+        int totalPurchases = 0;
+
+        // Input the number of purchases for each menu item
+        // Loop through menu items
+        for (int i = 0; i < numMenuItems; i++) {
+            int purchase = 0;
+            do {
+                // Prompts with index correction
+                System.out.print("Enter the number of purchases for item " + (i + 1) + ": ");
+
+                purchase = scanner.nextInt();
+                if (purchase <= 0) {
+                    System.out.println("Error: Number of purchases must be greater than zero.");
+                }
+            } while (purchase <= 0);
+
+            purchases[i] = purchase;
+            totalPurchases += purchase;
+        }
+
+
+
+
 
         // 2️⃣ PROCSSING SECTION
         // 3️⃣ CALCULATIONS
