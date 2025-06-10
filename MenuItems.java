54991e49f87319643463041b0256dd66d283bb71
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2025-06-10 22:16:49 +1000

    feat: populated MenuItems class with instance variables, constructors, setters and getters.

--- MenuItems.java
+++ MenuItems.java
@@ -1,3 +1,47 @@
 public class MenuItems {
+    // Instance variables
+    private String name;
+    private int purchases;
+    private double price;
     
+    // Default Constructor
+    public MenuItems() {
+        this.name = "Unknown";
+        this.purchases = 0;
+        this.price = 0.0;
+    }
+
+    // Parameterised Constructor
+    public MenuItems(String name, int purchases, double price) {
+        this.name = name;
+        this.purchases = 0;
+        this.price = 0.0;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPurchases(int purchases) {
+        this.purchases = purchases;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getPurchases() {
+        return purchases;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
+
